0d991b21d718bdcfcc2e38fe7edc314255f9a7c8
AlexKT1022 <alexkt1022@gmail.com>
2025-08-19 12:24:46 -0400

    added spinning loading icon

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,14 @@
+import { FaSpinner } from 'react-icons/fa';
+
 const Loading = () => {
-  return <h1>Loading...</h1>;
+  return (
+    <>
+      <FaSpinner
+        className="spin-icon"
+        size={100}
+      />
+    </>
+  );
 };
 
 export default Loading;
